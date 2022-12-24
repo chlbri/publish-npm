ca400200c7eb237ef8a32eb7d0608970fd8c33d6
chlbri <bri_lvi@icloud.com>
2022-12-24 18:48:05 +0000

    test (begin): commit Author: chlbri <bri_lvi@icloud.com>

--- src/commit.ts
+++ src/commit.ts
@@ -6,7 +6,6 @@ const _getLastCommit = promisify(getLastCommit);
 export async function getLastCommitText() {
   const commit = await _getLastCommit();
   commit; //?
-  // return `Last commit: ${commit.shortHash} - ${commit.subject}`;
 }
 
 getLastCommitText();
